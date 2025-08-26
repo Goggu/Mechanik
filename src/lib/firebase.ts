0f61e8493f45c002e7c9f4f71d3449520af23fb2
Gokul <GKcherath@gmail.com>
2025-08-26 08:37:55 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,15 +6,14 @@ import { getFirestore } from "firebase/firestore";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  "projectId": "helpnow-r5cll",
+  "appId": "1:571525603673:web:0f036ef3f289ac060d6c48",
+  "storageBucket": "helpnow-r5cll.firebasestorage.app",
+  "apiKey": "AIzaSyDvTubwY4voAhfia27K29jlhWx10BYLh_E",
+  "authDomain": "helpnow-r5cll.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "571525603673"
 };
 
 // Initialize Firebase
