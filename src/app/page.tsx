ec525a688e8d1dc24ee67d5d5d7f1d693d3d2eb8
Gokul <GKcherath@gmail.com>
2025-09-07 16:00:14 +0000

    alerter's view that we set before is the homepage that appears by defaul

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,11 @@
-
 "use client";
 
-import { useAuth } from "@/hooks/use-auth";
 import { AlerterView } from "@/components/alerter-view";
 
 export default function Home() {
-  const { user } = useAuth();
-
   return (
     <div className="container mx-auto text-center flex-grow flex items-center justify-center">
-      {user ? <AlerterView /> : null}
+      <AlerterView />
     </div>
   );
 }
